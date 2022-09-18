2145c37c0632181946c47329a9ce27731ce62116
Kirill_Kalubkin <kalibkin.kirill@mail.ru>
2022-09-18 22:22:17 +0300

    Выполненное домашнее задание №1

--- unit_one/lesson_2_16_09_2022/homework/task3.py
+++ unit_one/lesson_2_16_09_2022/homework/task3.py
@@ -3,3 +3,18 @@
 age = 36.6
 temperature = 25
 
+# 1 способ решения (через введение новой дополнительной переменной, допустим "reverse"):
+age = 36.6
+temperature = 25
+reverse = age
+age = temperature
+temperature = reverse
+print('age =', age)
+print('temperature =', temperature)
+
+# 2 способ решения (сразу переприсвоить переменные):
+age = 36.6
+temperature = 25
+age, temperature = temperature, age
+print('age =', age)
+print('temperature =', temperature)
