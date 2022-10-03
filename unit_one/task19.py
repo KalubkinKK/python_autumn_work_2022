6358021e542704ca31e4cbb43900b61f5ceca0a5
Kirill_Kalubkin <kalubkin.kirill@mail.ru>
2022-10-03 15:54:37 +0300

    Выполненное домашнее задание.

---
+++ unit_one/task19.py
@@ -0,0 +1,21 @@
+# #todo: Требуется создать csv-файл «algoritm.csv» со следующими столбцами:
+# – id - номер по порядку (от 1 до 10);
+# – текст из списка algoritm
+#
+# algoritm = [ "C4.5" , "k - means" , "Метод опорных векторов" , "Apriori" ,
+# "EM" , "PageRank" , "AdaBoost", "kNN" , "Наивный байесовский классификатор" , "CART" ]
+#
+# Каждое значение из списка должно находится на отдельной строке.
+
+algoritm = ["C4.5", "k - means", "Метод опорных векторов", "Apriori",
+            "EM", "PageRank", "AdaBoost", "kNN", "Наивный байесовский классификатор", "CART"]
+new_list = []
+title = ('ID\t', 'algoritm\n')
+
+fd = open('algoritm.csv', mode='tw', encoding='UTF-8')
+
+for i in range(len(algoritm)):
+    new_list.append(str(i + 1) + '   ' + algoritm[i] + '\n')
+
+fd.writelines(title)
+fd.writelines(new_list)
